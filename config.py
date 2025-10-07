410abaacf815487fbc47a32b437e5a56e057d42a
Harshil Awasthi <96411822+CodeBloodedDeveloper@users.noreply.github.com>
2025-10-07 10:13:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,11 +16,12 @@ except (ImportError, KeyError):
 
 # Models & Vector DB config
 EMBED_MODEL = "models/embedding-001"
-CHAT_MODEL = "gemini-1.5-flash-latest"
+CHAT_MODEL = "gemini-2.0-flash"
 VECTOR_DB_DIR = "./chroma_store"
 
 def get_genai_client():
     if not GEMINI_API_KEY:
         raise RuntimeError("❌ GEMINI_API_KEY missing. For local use, set it in .env. For Streamlit, add it to Secrets.")
     genai.configure(api_key=GEMINI_API_KEY)
+
     return genai
